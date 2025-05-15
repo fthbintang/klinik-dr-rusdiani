753926cd692ebb81c3ceca77a2cb7b5c42445b3a
fthbintang <bintangfathehah@gmail.com>
2025-05-15 23:40:24 +0800

    Apply auth middleware to protect dashboard and other routes

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
 
-Route::get('/', [LoginController::class, 'index'])->name('login');
+Route::get('/', [LoginController::class, 'index'])->name('login')->middleware('guest', 'prevent-back-history');
 Route::post('/sign-in', [LoginController::class, 'authenticate'])->name('authentication');
-
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::prefix('dashboard')->name('dashboard.')->group(function () {
+Route::prefix('dashboard')->name('dashboard.')->middleware('auth')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('index');
 });
