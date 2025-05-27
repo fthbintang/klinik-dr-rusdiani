c6afde8d5b575911453e58ed688a7898b32b06ad
fthbintang <bintangfathehah@gmail.com>
2025-05-27 23:47:55 +0800

    Add ObatController and SupplierController with index methods, create models and define relationship

---
+++ app/Models/Supplier.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Supplier extends Model
+{
+    protected $table = 'supplier';
+    protected $guarded = ['id'];
+
+    public function obat(): HasMany
+    {
+        return $this->hasMany(Obat::class);
+    }
+}
---
+++ resources/views/obat/supplier/index.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    Ini Halaman Supplier
+</x-layout>
