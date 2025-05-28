1dfba75daa1bd6458340cb270e9bb09e3575ad55
fthbintang <bintangfathehah@gmail.com>
2025-05-28 15:08:06 +0800

    Create Obat & Supplier view

--- resources/views/obat/index.blade.php
+++ resources/views/obat/index.blade.php
@@ -1,3 +1,90 @@
 <x-layout>
-    Ini Halaman Obat
+    <div class="page-heading">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Halaman {{ $title }}</h3>
+                </div>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header d-flex justify-content-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <a href="{{ route('index') }}">Home</a>
+                            </li>
+                            <li class="breadcrumb-item active" aria-current="page">
+                                {{ $title }}
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="section">
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-sm-8">
+                        <h5 class="card-title">Data {{ $title }}</h5>
+                    </div>
+                    <div class="col-sm-4 d-flex justify-content-end">
+                        <a href="{{ route('obat.supplier.index') }}" class="btn btn-light me-2">Supplier</a>
+                        <a href="#" class="btn btn-success">Tambah Obat</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table" id="table1">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Kategori</th>
+                                <th>Satuan</th>
+                                <th>Stok</th>
+                                <th>Harga</th>
+                                <th>Expired</th>
+                                <th>Supplier</th>
+                                <th>Keterangan</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($obat as $row)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $row->nama_obat }}</td>
+                                    <td>{{ $row->kategori }}</td>
+                                    <td>{{ $row->satuan }}</td>
+                                    <td>{{ $row->stok }}</td>
+                                    <td>{{ $row->harga }}</td>
+                                    <td>{{ $row->expired_date }}</td>
+                                    <td>{{ $row->supplier->nama_supplier ?? '-' }}</td>
+                                    <td>{{ $row->keterangan ?? '-' }}</td>
+                                    <td>
+                                        <a href="#" class="btn icon btn-info">
+                                            <i class="bi bi-eye-fill"></i>
+                                        </a>
+                                        <a href="#" class="btn icon btn-warning">
+                                            <i class="bi bi-pencil"></i>
+                                        </a>
+                                        <form action="#" method="POST" class="d-inline form-delete-user">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="button" class="btn icon btn-danger btn-delete-user">
+                                                <i class="bi bi-trash"></i>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
 </x-layout>
--- resources/views/obat/supplier/index.blade.php
+++ resources/views/obat/supplier/index.blade.php
@@ -1,3 +1,83 @@
 <x-layout>
-    Ini Halaman Supplier
+    <div class="page-heading">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Halaman {{ $title }}</h3>
+                </div>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header d-flex justify-content-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <a href="{{ route('index') }}">Home</a>
+                            </li>
+                            <li class="breadcrumb-item">
+                                <a href="{{ route('obat.index') }}">Obat</a>
+                            </li>
+                            <li class="breadcrumb-item active" aria-current="page">
+                                {{ $title }}
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="justify-content mb-3">
+        <a href="{{ route('obat.index') }}" class="btn btn-info float-end">Kembali</a>
+    </div>
+
+    <section class="section">
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-sm-8">
+                        <h5 class="card-title">Data {{ $title }}</h5>
+                    </div>
+                    <div class="col-sm-4 d-flex justify-content-end">
+                        <a href="#" class="btn btn-success">Tambah Supplier</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table" id="table1">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Telepon</th>
+                                <th>Alamat</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($supplier as $row)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $row->nama_supplier }}</td>
+                                    <td>{{ $row->telepon }}</td>
+                                    <td>{{ $row->alamat }}</td>
+                                    <td>
+                                        <a href="#" class="btn icon btn-warning">
+                                            <i class="bi bi-pencil"></i>
+                                        </a>
+                                        <form action="#" method="POST" class="d-inline form-delete-user">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="button" class="btn icon btn-danger btn-delete-user">
+                                                <i class="bi bi-trash"></i>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
 </x-layout>
