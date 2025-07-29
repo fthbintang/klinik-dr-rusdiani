b14b7a1ad43944195cccc3c92858cf9007b1494b
fthbintang <bintangfathehah@gmail.com>
2025-07-29 22:38:24 +0900

    Update a PasienFactory to make name of the patient is Indonesian name

--- database/factories/PasienFactory.php
+++ database/factories/PasienFactory.php
@@ -15,24 +15,26 @@ class PasienFactory extends Factory
 
     public function definition(): array
     {
+        $faker = \Faker\Factory::create('id_ID'); // Lokalisasi ke bahasa Indonesia
+
         $noRm = 'RM-' . str_pad(self::$increment, 2, '0', STR_PAD_LEFT);
         self::$increment++;
 
         return [
             'user_id' => User::factory(),
             'no_rm' => $noRm,
-            'nama_lengkap' => $this->faker->name(),
-            'nama_panggilan' => $this->faker->firstName(),
-            'nik' => $this->faker->unique()->numerify('################'),
-            'jenis_kelamin' => $this->faker->randomElement(['Laki-laki', 'Perempuan']),
-            'no_hp' => $this->faker->phoneNumber(),
-            'tempat_lahir' => $this->faker->city(),
-            'tanggal_lahir' => $this->faker->date('Y-m-d'),
-            'alamat' => $this->faker->address(),
-            'pekerjaan' => $this->faker->jobTitle(),
-            'status_perkawinan' => $this->faker->randomElement(['Belum Menikah', 'Menikah', 'Cerai']),
-            'golongan_darah' => $this->faker->randomElement(['A', 'B', 'AB', 'O']),
-            'agama' => $this->faker->randomElement(['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha', 'Konghucu']),
+            'nama_lengkap' => $faker->name(),
+            'nama_panggilan' => $faker->firstName(),
+            'nik' => $faker->unique()->numerify('################'),
+            'jenis_kelamin' => $faker->randomElement(['Laki-laki', 'Perempuan']),
+            'no_hp' => '08' . $this->faker->numerify('##########'),
+            'tempat_lahir' => $faker->city(),
+            'tanggal_lahir' => $faker->date('Y-m-d'),
+            'alamat' => $faker->address(),
+            'pekerjaan' => $faker->jobTitle(),
+            'status_perkawinan' => $faker->randomElement(['Belum Menikah', 'Menikah', 'Cerai']),
+            'golongan_darah' => $faker->randomElement(['A', 'B', 'AB', 'O']),
+            'agama' => $faker->randomElement(['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha', 'Konghucu']),
             'created_at' => now(),
             'updated_at' => now(),
         ];
